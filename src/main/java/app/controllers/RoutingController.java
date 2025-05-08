bd2ce2bef6f85bbb0099ce7b70a02ee22af63005
DDVoss <danieldvoss@gmail.com>
2025-05-08 11:20:36 +0200

    button now corresponds properly to the routing so you can navigate between pages. Images added so index is displayed properly.

--- src/main/java/app/controllers/RoutingController.java
+++ src/main/java/app/controllers/RoutingController.java
@@ -5,9 +5,11 @@ import io.javalin.Javalin;
 
 public class RoutingController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)    {
+        // render
         app.get("index", ctx -> ctx.render("index.html"));
         app.get("size", ctx -> ctx.render("size.html"));
         app.get("customer-information", ctx -> ctx.render("customer-information.html"));
         app.get("summary", ctx -> ctx.render("summary.html"));
+        app.get("plan-drawing", ctx -> ctx.render("plan-drawing.html"));
     }
 }
