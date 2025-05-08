450d97604c1e0090b5faeb53f7fcb7b40b3d92dd
Esben10 <esbenmoldt@gmail.com>
2025-05-08 12:29:07 +0200

    Udviklet Bom klasse og Materials klasse samt mapper for begge. Bom mangler stadig udregnings funktionalitet.

---
+++ src/main/java/app/entities/Material.java
@@ -0,0 +1,69 @@
+package app.entities;
+
+public class Material {
+    private int materialId;
+    private String description;
+    private String unit;
+    private String type;
+    private int pricePrUnit;
+
+
+    public Material(int materialId, String description, String unit, String type, int pricePrUnit) {
+        this.materialId = materialId;
+        this.description = description;
+        this.unit = unit;
+        this.type = type;
+        this.pricePrUnit = pricePrUnit;
+
+    }
+
+    public Material(String description, String unit, String type, int pricePrUnit) {
+        this.description = description;
+        this.unit = unit;
+        this.type = type;
+        this.pricePrUnit = pricePrUnit;
+    }
+
+
+
+
+    public int getMaterialId() {
+        return materialId;
+    }
+    public void setMaterialId(int materialId) {
+        this.materialId = materialId;
+    }
+
+
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getUnit() {
+        return unit;
+    }
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public int getPricePrUnit() {
+        return pricePrUnit;
+    }
+    public void setPricePrUnit(int pricePrUnit) {
+        this.pricePrUnit = pricePrUnit;
+    }
+}
---
+++ src/main/java/app/persistence/mappers/BomMapper.java
@@ -0,0 +1,39 @@
+package app.persistence.mappers;
+import app.entities.Bom;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static app.Main.connectionPool;
+
+public class BomMapper {
+
+    public static void createBom(Bom bom) throws SQLException, DatabaseException {
+        String sql = "INSERT INTO bom (variant_id, length, order_id) VALUES (?, ?, ?) RETURNING bom_id";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, bom.getVariantId());
+            ps.setInt(2, bom.getLength());
+            ps.setInt(3, bom.getOrderId());
+
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    int generatedId = rs.getInt("bom_id");
+                    bom.setBomId(generatedId);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e, "Error inserting bom");
+        }
+    }
+
+
+
+}
+
