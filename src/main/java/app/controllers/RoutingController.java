2129e2ba9fe7d071d6cd95ac7e1fc8307b4668c6
DDVoss <danieldvoss@gmail.com>
2025-05-08 12:45:40 +0200

    changed routing to send info from size.html to summary.html

--- src/main/java/app/controllers/RoutingController.java
+++ src/main/java/app/controllers/RoutingController.java
@@ -3,13 +3,34 @@ package app.controllers;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class RoutingController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)    {
         // render
         app.get("index", ctx -> ctx.render("index.html"));
         app.get("size", ctx -> ctx.render("size.html"));
         app.get("customer-information", ctx -> ctx.render("customer-information.html"));
-        app.get("summary", ctx -> ctx.render("summary.html"));
+
         app.get("plan-drawing", ctx -> ctx.render("plan-drawing.html"));
+
+        //posts
+
+        app.post("/size", ctx -> {
+            Map<String, String> sizeInfo = new HashMap<>();
+            sizeInfo.put("width", ctx.formParam("width"));
+            sizeInfo.put("length", ctx.formParam("length"));
+            sizeInfo.put("trapez", ctx.formParam("trapez"));
+            ctx.sessionAttribute("sizeInfo", sizeInfo);
+            ctx.redirect("/summary");
+        });
+
+        app.get("summary", ctx -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("sizeInfo", ctx.sessionAttribute("sizeInfo")); // Ensure session data is available
+            ctx.render("summary.html", model);
+        });
     }
 }
