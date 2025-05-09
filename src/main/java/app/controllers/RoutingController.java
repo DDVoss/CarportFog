0cbb3f27cf33dda2c6af0af1b00a5f4ece167892
DDVoss <danieldvoss@gmail.com>
2025-05-09 12:33:22 +0200

    Made a summary page work properly with information stored in a session hashmap.

--- src/main/java/app/controllers/RoutingController.java
+++ src/main/java/app/controllers/RoutingController.java
@@ -2,13 +2,14 @@ package app.controllers;
 
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
+import org.eclipse.jetty.server.session.Session;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class RoutingController {
-    public static void addRoutes(Javalin app, ConnectionPool connectionPool)    {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         // render
         app.get("index", ctx -> ctx.render("index.html"));
         app.get("size", ctx -> ctx.render("size.html"));
@@ -26,22 +27,33 @@ public class RoutingController {
             ctx.redirect("/plan-drawing");
         });
 
-        app.post("/personal", ctx -> {
+        app.post("/customer-information", ctx -> {
             Map<String, String> perInfo = new HashMap<>();
             perInfo.put("fname", ctx.formParam("fname"));
             perInfo.put("lname", ctx.formParam("lname"));
             perInfo.put("phone", ctx.formParam("phone"));
             perInfo.put("email", ctx.formParam("email"));
-            perInfo.put("address", ctx.formParam("adress"));
+            perInfo.put("address", ctx.formParam("address"));
             perInfo.put("zip", ctx.formParam("zip"));
             ctx.sessionAttribute("perInfo", perInfo);
             ctx.redirect("/summary");
         });
 
+
+        app.get("/size", ctx -> {
+            Map<String, Object> model = new HashMap<>();
+            Map<String, String> sizeInfo = ctx.sessionAttribute("sizeInfo");
+            model.put("sizeInfo", sizeInfo);
+            ctx.render("size.html", model);
+        });
+
+
         app.get("summary", ctx -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("sizeInfo", ctx.sessionAttribute("sizeInfo")); // Ensure session data is available
+            model.put("sizeInfo", ctx.sessionAttribute("sizeInfo"));
+            model.put("perInfo", ctx.sessionAttribute("perInfo"));
             ctx.render("summary.html", model);
         });
+
     }
 }
